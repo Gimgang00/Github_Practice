732463bd0d492570646c0056badc8ed0566fc129
Jacob (Gyeongju) Kim <abox00@naver.com>
2025-04-10 20:03:28 +0900

    프로젝트 만듬

---
+++ gitTest/gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Jacob on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
